dab9c3f76105a380bb292fc08d4e05c93c877a70
Anuj <abgodase@iu.edu>
2020-10-24 11:41:26 -0400

    updated remote links

--- start_kvstore.sh
+++ start_kvstore.sh
@@ -2,6 +2,6 @@
 
 cd /home/anuj/
 rm -rf MapReduceOnGCP
-git clone https://github.iu.edu/abgodase/MapReduceOnGCP.git
+git clone https://github.com/1byxero/MapReduceOnGCP.git
 cd MapReduceOnGCP
 ./start_kv_store.py 0.0.0.0 8888
--- start_reducerpc.sh
+++ start_reducerpc.sh
@@ -2,6 +2,6 @@
 
 cd /home/anuj/
 rm -rf MapReduceOnGCP
-git clone https://github.iu.edu/abgodase/MapReduceOnGCP.git
+git clone https://github.com/1byxero/MapReduceOnGCP.git
 cd MapReduceOnGCP
 ./reducerpc.py 0.0.0.0 8888
